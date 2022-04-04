1b339e639923f6f02e64b72a3a5c4be9cf9d0a92
daryllaraya <daryllaraya@gmail.com>
2022-04-04 14:52:07 +0800

    modified the sass partials/architecture

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
 
 function buildStyles() {
-  return src('lp/**/*.scss')
+  return src('sass/**/*.scss')
     .pipe(sass())
     .pipe(autoprefixer({ browsers: ['last 2 versions'], cascade: false }))
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['lp/**/*.scss', '*.html'], buildStyles)
+  watch(['sass/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
